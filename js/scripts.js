7c0e1eb53b693f9d1da4cdbbf5996dc8152a02d7
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2022-09-18 18:34:00 -0300

    aplicando a api no projeto

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,35 @@
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+let imagePokemon = document.querySelector("#image-pokemon")
+let info = document.querySelector("#info")
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+button.addEventListener("click", (e) => {
+
+    e.preventDefault()
+
+    const api = `https://pokeapi.co/api/v2/pokemon/${input.value}`
+
+    console.log(api)
+
+    fetch(api)
     .then(response => response.json())
     .then(data => {
         console.log(data)
+        imagePokemon.innerHTML = `<img src="${data.sprites.front_default}"/>`
+        info.innerHTML = `
+        
+        <span>Nome: ${data.name}</span>
+        <span>Tipo: ${data.types[0].type.name}</span>
+
+        `
+        console.log(info)
+
+        input.value = ""
+        input.focus()
+        
     })
+
+})
+
+
+
